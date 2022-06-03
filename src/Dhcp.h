48aee23b2c5ae56703946e1b1e8d86863875c27a
Hagen Burchardt <hagen@isx.de>
2022-06-03 07:29:29 +0200

    Added hostname argument

--- src/Dhcp.h
+++ src/Dhcp.h
@@ -180,7 +180,7 @@ public:
   IPAddress getDhcpServerIp();
   IPAddress getDnsServerIp();
   
-  int beginWithDHCP(uint8_t *, unsigned long timeout = 60000, unsigned long responseTimeout = 4000);
+  int beginWithDHCP(uint8_t *, unsigned long timeout = 60000, unsigned long responseTimeout = 4000, const char *hostname);
   int checkLease();
 };
 
